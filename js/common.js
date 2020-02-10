ef05515c2e3cfa3a6f4ffe66e368854960e18080
ITkah <ilatkacenko132@gmail.com>
2020-02-11 00:05:09 +0200

    slider-bug

--- js/common.js
+++ js/common.js
@@ -1,2 +1,11 @@
-console.log("hello");
-console.log("hello");
+$( document ).ready(function() {
+    str = $('.short-text-1').html();
+    str = str.substr(0,270) + '...';
+    $('.short-text-1').html(str);
+
+    $('div.tabs__caption').on('click', 'p:not(.active-tab)', function() {
+        $(this)
+        .addClass('active-tab').siblings().removeClass('active-tab')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+});
