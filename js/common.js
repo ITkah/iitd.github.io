0a464524e7e0377b4b8cef0ca6893d593a8dcd5c
ITkah <ilatkacenko132@gmail.com>
2020-02-11 02:05:32 +0200

    desk-version done

--- js/common.js
+++ js/common.js
@@ -1,11 +1,19 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     str = $('.short-text-1').html();
-    str = str.substr(0,270) + '...';
+    str = str.substr(0, 270) + '...';
     $('.short-text-1').html(str);
 
     $('div.tabs__caption').on('click', 'p:not(.active-tab)', function() {
         $(this)
-        .addClass('active-tab').siblings().removeClass('active-tab')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            .addClass('active-tab').siblings().removeClass('active-tab')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $('.single-item').slick({
+        dots: true,
+        arrows: false,
+        speed: 200,
+        fade: true,
+        cssEase: 'linear'
     });
 });
