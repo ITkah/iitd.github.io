9a78bfd2ba2609c4aa1917f1e255ffc7b52f23a2
ITkah <ilatkacenko132@gmail.com>
2020-02-20 13:37:32 +0200

    fix menu

--- js/common.js
+++ js/common.js
@@ -6,6 +6,42 @@ $(document).ready(function() {
             .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
 
+    $(".btn-menu").on("click", function(){
+        $("nav").fadeIn(100);
+    });
+
+    $(".close-btn").on("click", function(){
+        $("nav").fadeOut(100);
+    });
+
+    $(".link-hover").hover(
+        function() {
+            $(".nav-box-dropdown-hover").fadeOut(0);
+            $(this).next().fadeIn(100);
+        }
+    );
+
+    $(".nav-box-dropdown-hover").mouseleave(function(){
+        $(".nav-box-dropdown-hover").fadeOut(100);
+    });
+
+    $(window).resize(function() {
+        if ($(window).width() < 990) {
+            $(".nav-box-dropdown-hover").unbind('mouseenter mouseleave');
+            $(".link-hover").unbind('mouseenter mouseleave');
+            $(".link-call").on("click", function(e){
+                e.preventDefault();
+                $(this).next().slideToggle(100);
+            });
+        }
+    });
+
+
+    $(".nav-box-title").on("click", function(){
+        $(this).siblings(".box-hide-nav").slideToggle(100);
+    });
+
+
     $('.single-item').slick({
         dots: true,
         arrows: false,
